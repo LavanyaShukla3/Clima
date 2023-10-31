ed20bff08597c3419742aca1f0e76d22211f7c62
Lavanya Shukla <112153884+LavanyaShukla3@users.noreply.github.com>
2023-10-31 12:54:32 +0530

    Update city_screen.dart

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -2,70 +2,59 @@ import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
 
 class CityScreen extends StatefulWidget {
+  const CityScreen({Key? key}) : super(key: key);
+
   @override
   _CityScreenState createState() => _CityScreenState();
 }
 
 class _CityScreenState extends State<CityScreen> {
-  String cityName='';
+  String cityName = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage('images/city_background.jpg'),
             fit: BoxFit.cover,
           ),
         ),
-        constraints: BoxConstraints.expand(),
+        constraints: const BoxConstraints.expand(),
         child: SafeArea(
           child: Column(
             children: <Widget>[
               Align(
                 alignment: Alignment.topLeft,
-                child: ElevatedButton(
+                child: TextButton(
                   onPressed: () {
                     Navigator.pop(context);
                   },
-                  child: Icon(
+                  child: const Icon(
                     Icons.arrow_back_ios,
+                    color: Colors.white,
                     size: 50.0,
                   ),
                 ),
               ),
               Container(
-                padding: EdgeInsets.all(20.0),
+                padding: const EdgeInsets.all(20.0),
                 child: TextField(
-                  style: TextStyle(
+                  decoration: kTextFieldInputDecoration,
+                  style: const TextStyle(
                     color: Colors.black,
                   ),
-                  decoration: InputDecoration(
-                    filled:true,
-                    fillColor: Colors.white,
-                    icon:Icon(
-                      Icons.location_city;
-                      color:Colors.white,
-                    ),
-                    hintText: 'Enter City Name',
-                    hintStyle: TextStyle(
-                      color: Colors.grey,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0),
-                      )
-                    ),
-                  ),
                   onChanged: (value) {
-                    cityName=value;
+                    cityName = value;
                   },
                 ),
               ),
-              ElevatedButton(
+              TextButton(
                 onPressed: () {
                   Navigator.pop(context, cityName);
                 },
-                child: Text(
+                child: const Text(
                   'Get Weather',
                   style: kButtonTextStyle,
                 ),
